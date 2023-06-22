fc8c514b012dcebc35213a3049f0f1b79555c15f
EmOn <emonmazumder000@gmail.com>
2023-06-22 11:34:29 +0600

    Inner_iframe

---
+++ src/main/java/com/selenium/Inner_iframe.java
@@ -0,0 +1,33 @@
+package com.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Inner_iframe {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://demo.automationtesting.in/Frames.html");
+
+        driver.findElement(By.xpath("//a[contains(text(),'Iframe with in an Iframe')]")).click();
+
+        // <-----Switch to outer frame----->
+        WebElement outerFrame = driver.findElement(By.xpath("//*[@id='Multiple']/iframe"));
+        driver.switchTo().frame(outerFrame); //Assigned frame as a WebElement
+
+        // <-----Switch to inner frame----->
+        WebElement innerFrame = driver.findElement(By.xpath("/html/body/section/div/div/iframe"));
+        driver.switchTo().frame(innerFrame);
+
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Hello World");
+
+
+    }
+    
+}
