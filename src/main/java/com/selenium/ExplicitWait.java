fd8db4dd4f6e3fa785448f3db2ebff5c696abc67
EmOn <emonmazumder000@gmail.com>
2023-07-15 23:55:26 +0600

    ExplicitWait

--- src/main/java/com/selenium/ExplicitWait.java
+++ src/main/java/com/selenium/ExplicitWait.java
@@ -2,13 +2,11 @@ package com.selenium;
 
 import java.time.Duration;
 
-import org.apache.hc.core5.util.Timeout;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
